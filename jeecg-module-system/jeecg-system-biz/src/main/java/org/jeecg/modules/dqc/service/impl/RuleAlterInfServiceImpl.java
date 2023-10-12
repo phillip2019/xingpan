4be909f8843acc14474478ba5f974af8687990c8
xiaowei.song <songxiaowei@chinagoods.com>
2023-10-12 11:38:05 +0800

    #Updates#dqc更改数据源

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/dqc/service/impl/RuleAlterInfServiceImpl.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/dqc/service/impl/RuleAlterInfServiceImpl.java
@@ -1,5 +1,7 @@
 package org.jeecg.modules.dqc.service.impl;
 
+import com.baomidou.dynamic.datasource.annotation.DS;
+import lombok.extern.slf4j.Slf4j;
 import org.jeecg.modules.dqc.entity.RuleAlterInf;
 import org.jeecg.modules.dqc.mapper.RuleAlterInfMapper;
 import org.jeecg.modules.dqc.service.IRuleAlterInfService;
@@ -13,7 +15,9 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
  * @Date:   2023-10-12
  * @Version: V1.0
  */
+@DS("griffin")
 @Service
+@Slf4j
 public class RuleAlterInfServiceImpl extends ServiceImpl<RuleAlterInfMapper, RuleAlterInf> implements IRuleAlterInfService {
 
 }
