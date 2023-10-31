cca4ff4ef818cb67a8a80cbc6e8b70680d46610d
xiaowei.song <songxiaowei@chinagoods.com>
2023-10-31 15:25:57 +0800

    #Updates#埋点验证开发完成

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/et/websocket/TeUaeChinagoodsWebSocket.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/et/websocket/TeUaeChinagoodsWebSocket.java
@@ -7,26 +7,22 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.PartitionInfo;
-import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.shiro.util.CollectionUtils;
-import org.jeecg.common.constant.WebsocketConst;
-import org.jeecg.common.modules.redis.client.JeecgRedisClient;
 import org.jeecg.modules.et.entity.EtChinagoods;
 import org.jeecg.modules.et.entity.EventTracking;
 import org.jeecg.modules.et.entity.UaeWSParamChinagoods;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.annotation.Resource;
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.text.ParseException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 /**
@@ -47,26 +43,30 @@ public class TeUaeChinagoodsWebSocket {
 
     @Value("${cg.et.topic}")
     private String sourceTopic = "events-tracking";
-    
-    /**线程安全Map*/
+
+    /**
+     * 线程安全Map
+     */
     private static final ConcurrentHashMap<String, Session> SESSION_POOL = new ConcurrentHashMap<>();
-    private static final ConcurrentHashMap<String, Boolean> SESSION_POOL_KAFKA_CONSUMER_FLAG_MAP = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, AtomicBoolean> SESSION_POOL_KAFKA_CONSUMER_FLAG_MAP = new ConcurrentHashMap<>();
+
+    private static ExecutorService THREAD_POOL = Executors.newCachedThreadPool();
 
 
     //==========【websocket接受、推送消息等方法 —— 具体服务节点推送ws消息】========================================================================================
     @OnOpen
     public void onOpen(Session session,
                        @PathParam(value = "userId") String userId
-                       ) throws ParseException {
+    ) throws ParseException {
         try {
             SESSION_POOL.put(userId, session);
-            SESSION_POOL_KAFKA_CONSUMER_FLAG_MAP.put(userId, Boolean.FALSE);
+            SESSION_POOL_KAFKA_CONSUMER_FLAG_MAP.put(userId, new AtomicBoolean(false));
             log.info("【埋点系统 WebSocket】有新的连接，总数为:" + SESSION_POOL.size());
         } catch (Exception e) {
             log.error("【埋点系统 WebSocket】有新的连接失败, userId: {}", userId, e);
         }
 
-        String queryString = session.getQueryString();
+        String queryString = session.getQueryString() + "&";
         String ip = StringUtils.substringBetween(queryString, "ip=", "&");
         String distinctId = StringUtils.substringBetween(queryString, "distinctId=", "&");
         String anonymousId = StringUtils.substringBetween(queryString, "anonymousId=", "&");
@@ -83,8 +83,17 @@ public class TeUaeChinagoodsWebSocket {
     }
 
     @OnClose
-    public void onClose(@PathParam("userId") String userId) {
+    public void onClose(@PathParam("userId") String userId) throws InterruptedException {
+        log.info("【系统 WebSocket】连接断开，用户编号为: {}", userId);
+
         if (StringUtils.isNotBlank(userId)) {
+            // 关闭kafka消费
+            if (SESSION_POOL_KAFKA_CONSUMER_FLAG_MAP.containsKey(userId)) {
+                SESSION_POOL_KAFKA_CONSUMER_FLAG_MAP.get(userId).set(false);
+            }
+            // 阻塞300ms，等待kafka消费线程结束
+            Thread.sleep(300);
+
             SESSION_POOL.remove(userId);
             SESSION_POOL_KAFKA_CONSUMER_FLAG_MAP.remove(userId);
         }
@@ -94,7 +103,7 @@ public class TeUaeChinagoodsWebSocket {
     /**
      * ws推送消息
      *
-     * @param userId 用户编号
+     * @param userId           用户编号
      * @param etChinagoodsList 埋点事件列表
      */
     public void pushMessage(String userId, List<EtChinagoods> etChinagoodsList) {
@@ -104,17 +113,19 @@ public class TeUaeChinagoodsWebSocket {
             if (etChinagoodsList != null && !etChinagoodsList.isEmpty()) {
                 try {
                     //update-begin-author:taoyan date:20211012 for: websocket报错 https://gitee.com/jeecg/jeecg-boot/issues/I4C0MU
-                    synchronized (session){
+                    synchronized (session) {
                         String message = JSONObject.toJSONString(etChinagoodsList);
                         log.debug("【系统 WebSocket】推送单人消息:" + message);
-                        session.getBasicRemote().sendText(message);
+                        if (session.isOpen()) {
+                            session.getBasicRemote().sendText(message);
+                        }
                     }
                     //update-end-author:taoyan date:20211012 for: websocket报错 https://gitee.com/jeecg/jeecg-boot/issues/I4C0MU
                 } catch (Exception e) {
                     log.error("【埋点验证】消息推送单人: {}, 消息失败", userId, e);
                 }
             } else {
-                log.error("【埋点验证】消息推送单人: {}, 消息为空", userId);
+                log.debug("【埋点验证】消息推送单人: {}, 消息为空", userId);
             }
         }
     }
@@ -133,13 +144,13 @@ public class TeUaeChinagoodsWebSocket {
                 Session session = item.getValue();
                 try {
                     //update-begin-author:taoyan date:20211012 for: websocket报错 https://gitee.com/jeecg/jeecg-boot/issues/I4C0MU
-                    synchronized (session){
+                    synchronized (session) {
                         log.debug("【系统 WebSocket】推送单人消息:" + message);
                         session.getBasicRemote().sendText(message);
                     }
                     //update-end-author:taoyan date:20211012 for: websocket报错 https://gitee.com/jeecg/jeecg-boot/issues/I4C0MU
                 } catch (Exception e) {
-                    log.error(e.getMessage(),e);
+                    log.error(e.getMessage(), e);
                 }
             }
         }
@@ -149,19 +160,20 @@ public class TeUaeChinagoodsWebSocket {
      * ws接受客户端消息
      */
     @OnMessage
-    public void onMessage(String message, @PathParam(value = "userId") String userId) throws ParseException {
-        if(!"ping".equals(message) && !WebsocketConst.CMD_CHECK.equals(message)){
-            log.info("【系统 WebSocket】收到客户端消息:" + message);
-        } else {
-            log.debug("【系统 WebSocket】收到客户端消息:" + message);
+    public void onMessage(String message, @PathParam(value = "userId") String userId) throws ParseException, InterruptedException {
+        // 消息体为json字符串，解析消息体，若消息体对象中type等于close_connection，则执行close方法，做清理操作
+        JSONObject jn = JSONObject.parseObject(message);
+        if ("close_connection".equals(jn.getString("type"))) {
+            this.onClose(userId);
         }
+        log.info("【系统 WebSocket】收到客户端消息: {}", message);
         // 若kafka实例已经初始化，则后续代码不需要执行
-        if (SESSION_POOL_KAFKA_CONSUMER_FLAG_MAP.get(userId)) {
+        if (SESSION_POOL_KAFKA_CONSUMER_FLAG_MAP.get(userId).get()) {
             JSONObject obj = new JSONObject();
             //业务类型
-            obj.put(WebsocketConst.MSG_CMD, WebsocketConst.CMD_CHECK);
+            obj.put("code", 200);
             //消息内容
-            obj.put(WebsocketConst.MSG_TXT, "心跳响应");
+            obj.put("msg", "success");
             this.pushMessageString(userId, obj.toJSONString());
         }
     }
@@ -190,56 +202,68 @@ public class TeUaeChinagoodsWebSocket {
      **/
     public void pollMessage(String userId, UaeWSParamChinagoods wsParamChinagoods) throws ParseException {
 
-        String distinctId = wsParamChinagoods.getDistinctId();
-        String anonymousId = wsParamChinagoods.getAnonymousId();
-        String ip = wsParamChinagoods.getIp();
-        try (KafkaConsumer<String, String> kafkaConsumer = initKafkaConsumer(userId,500)) {
-
-            SESSION_POOL_KAFKA_CONSUMER_FLAG_MAP.put(userId, Boolean.TRUE);
-
-            // 判断当前用户会话是否存在，若还存在则继续循环
-            while (SESSION_POOL.containsKey(userId)) {
-                // 100 是超时时间（ms），在该时间内 poll 会等待服务器返回数据
-                ConsumerRecords<String, String> records = kafkaConsumer.poll(3000);
-                log.debug("批量获取消息，当前获取消息大小为： {}", records.count());
-
-                // poll 返回一个记录列表。
-                // 每条记录都包含了记录所属主题的信息、记录所在分区的信息、记录在分区里的偏移量，以及记录的键值对。
-                List<EtChinagoods> tmpResultList = Streams.stream(records)
-                        .map(record -> EventTracking.of(record.value()))
-                        .map(EventTracking::toChinagoods)
-                        .filter(et -> {
-                            boolean ret = false;
-                            if (StringUtils.isNotBlank(distinctId)) {
-                                ret = StringUtils.equals(distinctId, et.getDistinctId());
-                                if (!ret) {
-                                    return false;
+        // 创建一个新线程来处理消息消费
+        THREAD_POOL.execute(() -> {
+            String distinctId = wsParamChinagoods.getDistinctId();
+            String anonymousId = wsParamChinagoods.getAnonymousId();
+            String ip = wsParamChinagoods.getIp();
+            try (KafkaConsumer<String, String> kafkaConsumer = initKafkaConsumer(userId, 500)) {
+                SESSION_POOL_KAFKA_CONSUMER_FLAG_MAP.get(userId).set(true);
+                // 判断当前用户会话是否存在，若还存在则继续循环
+                while (SESSION_POOL_KAFKA_CONSUMER_FLAG_MAP.containsKey(userId) && SESSION_POOL_KAFKA_CONSUMER_FLAG_MAP.get(userId).get()) {
+                    // 100 是超时时间（ms），在该时间内 poll 会等待服务器返回数据
+                    ConsumerRecords<String, String> records = kafkaConsumer.poll(3000);
+                    // poll 返回一个记录列表。
+                    // 每条记录都包含了记录所属主题的信息、记录所在分区的信息、记录在分区里的偏移量，以及记录的键值对。
+                    List<EtChinagoods> tmpResultList = Streams.stream(records)
+                            .map(record -> EventTracking.of(record.value()))
+                            .map(EventTracking::toChinagoods)
+                            .filter(et -> {
+                                boolean ret = false;
+                                if (StringUtils.isNotBlank(distinctId)) {
+                                    ret = StringUtils.equals(distinctId, et.getDistinctId());
+                                    if (!ret) {
+                                        return false;
+                                    }
                                 }
-                            }
 
-                            if (StringUtils.isNotBlank(anonymousId)) {
-                                ret = StringUtils.equals(anonymousId, et.getAnonymousId());
-                                if (!ret) {
-                                    return false;
+                                if (StringUtils.isNotBlank(anonymousId)) {
+                                    ret = StringUtils.equals(anonymousId, et.getAnonymousId());
+                                    if (!ret) {
+                                        return false;
+                                    }
                                 }
-                            }
 
-                            if (StringUtils.isNotBlank(ip)) {
-                                ret = StringUtils.equals(ip, et.getIp());
-                                if (!ret) {
-                                    return false;
+                                if (StringUtils.isNotBlank(ip)) {
+                                    ret = StringUtils.equals(ip, et.getIp());
+                                    if (!ret) {
+                                        return false;
+                                    }
                                 }
-                            }
-                            return ret;
-                        }).collect(Collectors.toList());
+                                log.debug("UserId: {}，过滤参数为，distinctId: {}，anonymousId: {}， ip: {}", userId, distinctId, anonymousId, ip);
+                                return ret;
+                            })
+                            // 升序
+                            .sorted(Comparator.comparing(EtChinagoods::getCreatedAt)).collect(Collectors.toList());
 
-                this.pushMessage(userId, tmpResultList);
+                    if (!tmpResultList.isEmpty()) {
+                        log.debug("埋点事件，当前获取埋点事件条数： {}", tmpResultList.size());
+                        this.pushMessage(userId, tmpResultList);
+                    }
+                }
+            } catch (Exception e) {
+                SESSION_POOL.remove(userId);
+                SESSION_POOL_KAFKA_CONSUMER_FLAG_MAP.remove(userId);
+                // 若异常为InterruptedException，则直接关闭，不打印异常信息和推送异常信息，若为其它，则打印异常和推送异常消息
+                if (e instanceof org.apache.kafka.common.errors.InterruptException) {
+                    return;
+                }
+
+                log.error("【系统 WebSocket】批量获取消息失败，发生异常为: ", e);
+                // 若无数据，则传递null，表明发生异常
+                this.pushMessage(userId, null);
             }
-        } catch (Exception e) {
-            log.error("【系统 WebSocket】批量获取消息失败，发生异常为: ", e);
-            // 若无数据，则传递null，表明发生异常
-            this.pushMessage(userId, null);
-        }
+        });
     }
 
     /**
