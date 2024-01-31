5ff1d52985240ae1998ec5153437cee25533c1d1
phillip2019 <msn734506700@live.com>
2024-01-31 22:07:33 +0800

    #Updates#完善页面路径标准化

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/et/entity/WebPathMapPip.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/et/entity/WebPathMapPip.java
@@ -67,4 +67,20 @@ public class WebPathMapPip implements Serializable {
 	@Excel(name = "参数", width = 15)
     @ApiModelProperty(value = "参数")
     private String parameter;
+    /**创建人*/
+    @ApiModelProperty(value = "创建人")
+    private java.lang.String createBy;
+    /**创建时间*/
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "创建时间")
+    private java.util.Date createTime;
+    /**更新人*/
+    @ApiModelProperty(value = "更新人")
+    private java.lang.String updateBy;
+    /**更新时间*/
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "更新时间")
+    private java.util.Date updateTime;
 }
