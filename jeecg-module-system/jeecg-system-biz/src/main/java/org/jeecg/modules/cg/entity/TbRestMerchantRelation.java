7b1e34bcde106e26d1a67beafdbd8a9b4e488fc7
zhaoyulonggt <zhaoyulong@chinagoods.com>
2025-06-06 10:00:21 +0800

    #Update修改导出字段

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/cg/entity/TbRestMerchantRelation.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/cg/entity/TbRestMerchantRelation.java
@@ -36,31 +36,35 @@ public class TbRestMerchantRelation implements Serializable {
     @ApiModelProperty(value = "主键")
     private String id;
 	/**创建人*/
+    @Excel(name = "创建人", width = 40)
     @ApiModelProperty(value = "创建人")
     private String createBy;
 	/**创建日期*/
+    @Excel(name = "创建日期", width = 40)
 	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty(value = "创建日期")
     private Date createTime;
 	/**更新人*/
+    @Excel(name = "更新人", width = 40)
     @ApiModelProperty(value = "更新人")
     private String updateBy;
 	/**更新日期*/
+    @Excel(name = "更新日期", width = 40)
 	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty(value = "更新日期")
     private Date updateTime;
 	/**设备端档口名称*/
-	@Excel(name = "设备端档口名称", width = 15)
+	@Excel(name = "设备端档口名称", width = 40)
     @ApiModelProperty(value = "设备端档口名称")
     private String deviceMerchantName;
 	/**展示档口名称*/
-	@Excel(name = "展示档口名称", width = 15)
+	@Excel(name = "展示档口名称", width = 40)
     @ApiModelProperty(value = "展示档口名称")
     private String merchantName;
 	/**食堂名称*/
-	@Excel(name = "食堂名称", width = 15)
+	@Excel(name = "食堂名称", width = 40)
     @ApiModelProperty(value = "食堂名称")
     private String restName;
 }
