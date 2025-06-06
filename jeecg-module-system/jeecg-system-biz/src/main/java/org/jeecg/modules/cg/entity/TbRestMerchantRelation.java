d4865661f2eded17ee499c7f8bce0c73caa4bdfd
zhaoyulonggt <zhaoyulong@chinagoods.com>
2025-06-06 10:53:37 +0800

    #Update修改导出excel字段

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/cg/entity/TbRestMerchantRelation.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/cg/entity/TbRestMerchantRelation.java
@@ -36,21 +36,17 @@ public class TbRestMerchantRelation implements Serializable {
     @ApiModelProperty(value = "主键")
     private String id;
 	/**创建人*/
-    @Excel(name = "创建人", width = 40)
     @ApiModelProperty(value = "创建人")
     private String createBy;
 	/**创建日期*/
-    @Excel(name = "创建日期", width = 40)
 	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty(value = "创建日期")
     private Date createTime;
 	/**更新人*/
-    @Excel(name = "更新人", width = 40)
     @ApiModelProperty(value = "更新人")
     private String updateBy;
 	/**更新日期*/
-    @Excel(name = "更新日期", width = 40)
 	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty(value = "更新日期")
